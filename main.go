20811ec4d06553b724128d25d9b88157ab5e8b66
William <williamsoares058@gmail.com>
2022-12-27 19:05:05 -0300

    projeto go unialfa

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/WilliamSoaress/curso-go/domain"
+	"github.com/WilliamSoaress/curso-go/domain/batizado"
+	"github.com/WilliamSoaress/curso-go/domain/casamento"
+	"github.com/WilliamSoaress/curso-go/domain/churrasco"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/churrasco", calculaFesta(churrasco.NewChurrasco()))
+	r.Post("/batizado", calculaFesta(batizado.NewBatizado()))
+	r.Post("/casamento", calculaFesta(casamento.NewCasamento()))
+	http.ListenAndServe(":3000", r)
+}
+
+func calculaFesta(s domain.Festa) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var param domain.Parametros
+		err := json.NewDecoder(r.Body).Decode(&param)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		ch, err := s.Calcula(param)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		j, err := ch.ToJSON()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		_, err = w.Write(j)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+	}
+}
